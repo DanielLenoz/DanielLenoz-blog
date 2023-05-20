547a8a96cc87280a54696597df23953f797ff5e3
DanielLenoz <danielstivenxx@outlook.com>
2023-05-19 21:35:42 -0500

    corrigiendo sintaxis del navar del home

--- src/style/js/template/template-navar-footer/navar/call-navar-home.js
+++ src/style/js/template/template-navar-footer/navar/call-navar-home.js
@@ -1,12 +1,12 @@
-const headerHome = document.querySelector("header");
+const headerHome = document.querySelector("#home");
 
 elementsNavarAndFooter.map((data) => {
   const navarMobile = new Navar(data);
-  const navarElement = navarMobile.navarMobile(); // Llamada correcta de la función
+  const navarElement = navarMobile.navarMobile();
   headerHome.appendChild(navarElement);
 });
 elementsNavarAndFooter.map((data) => {
   const navarDesktop = new Navar(data);
-  const navarElement = navarDesktop.navarDesktop(); // Llamada correcta de la función
+  const navarElement = navarDesktop.navarDesktop();
   headerHome.appendChild(navarElement);
 });
